1b016c3abaa9bc42d88a3413afb476913f64f1bb
Alexander Gorbovets <alexander.gorbovets@gmail.com>
2021-11-05 23:32:27 +0300

    Implemented saving links

--- backend/app/Http/Controllers/LinkController.php
+++ backend/app/Http/Controllers/LinkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
 use Illuminate\Http\Request;
 
 class LinkController extends Controller
@@ -10,8 +11,22 @@ class LinkController extends Controller
     function shorten(Request $request) {
         $baseUrl = $request->getSchemeAndHttpHost() . $request->getBasePath();
 
+        $link = new Link();
+        $link->original_url = $request->json()->get('url');
+        $link->code = '';
+        $link->visit_count = 0;
+        $link->save();
+
+        try {
+            $code = $link->id * 100000 + random_int(0, 99999);
+            $code = base_convert($code, 10, 36);
+            $link->code = $code;
+            $link->save();
+        } catch (\Exception $e) {
+        }
+
         return json_encode([
-            'result' => $baseUrl . '/da98weg3'
+            'result' => $baseUrl . '/' . $link->code
         ]);
     }
 
--- backend/app/Models/Link.php
+++ backend/app/Models/Link.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $original_url
+ * @property mixed|string $code
+ */
 class Link extends Model
 {
     use HasFactory;
