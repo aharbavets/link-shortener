f56293fd81a74364182b35fa111d9eabdb418e3a
Alexander Gorbovets <alexander.gorbovets@gmail.com>
2021-11-05 15:59:49 +0300

    Implemented basic UI behavior

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LinkShortener/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LinkShortener from './components/LinkShortener.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LinkShortener
   }
 }
 </script>
@@ -19,8 +18,19 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height: 100%;
+}
+
+
+
+html {
+  height: 100%;
+}
+
+body {
+  font-family: 'Nunito', sans-serif;
+  height: 100%;
 }
 </style>
---
+++ frontend/src/components/LinkShortener.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container center-vertically" id="container">
+
+    <div class="content">
+      <h1 class="display-4">Link Shortener</h1>
+
+      <form v-if="!shortened" @submit.stop="shorten">
+        <div class="form-group" >
+          <label for="original_link">Enter your long link here</label>
+          <div class="input-group mb-3">
+            <input required type="url" class="form-control" v-model="originalLink"  id="original_link" aria-describedby="original_link" placeholder="Your link">
+            <div class="input-group-append">
+              <button type="submit" class="btn btn-primary">Shorten</button>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      <div v-if="shortened">
+        <div class="alert alert-success" role="alert">
+          Your link has been shortened:
+        </div>
+
+        <div class="input-group mb-3">
+          <input readonly type="url" class="form-control" aria-describedby="original_link">
+          <div class="input-group-append">
+            <button type="submit" class="btn btn-primary">Copy</button>
+          </div>
+        </div>
+
+        <button type="button" class="btn btn-light" @click="shortenAgain">Shorten another link</button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LinkShortener',
+  props: {
+  },
+  data() {
+    return {
+      shortened: false,
+      originalLink: ''
+    }
+  },
+  methods: {
+    shorten() {
+      // todo actually shorten the link
+
+      this.originalLink = ''
+      this.shortened = true
+    },
+
+    shortenAgain() {
+      this.shortened = false
+    },
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+}
+
+.content {
+  width: 600px;
+}
+
+.center-vertically {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
